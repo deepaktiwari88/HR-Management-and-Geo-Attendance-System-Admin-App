82a85f6036118cb916bd0327f90c99f6c0cc694c
Deepak Tiwari <tiwarid99@gmail.com>
2019-12-02 19:46:01 +0530

    Added Dialog on user add or error

--- lib/ui/adduser.dart
+++ lib/ui/adduser.dart
@@ -40,8 +40,7 @@ class _AddUSerState extends State<AddUser> {
   Auth authObject = Auth();
 
   @override
-  void initState(){
-
+  void initState() {
     _managerRef = db.reference().child('managers');
     _officeRef = db.reference().child('location');
     _userRef = db.reference().child('users');
@@ -54,29 +53,22 @@ class _AddUSerState extends State<AddUser> {
   }
 
   void _getOffices() async {
-
-    _officeRef.once().then((DataSnapshot snapshot){
-      
-      
+    _officeRef.once().then((DataSnapshot snapshot) {
       setState(() {
         officeList = new Map<String, dynamic>.from(snapshot.value);
         officeKeys = officeList.keys.toList();
         officeHintText = "Choose Location";
       });
-      
     });
   }
 
   void _getManagers() async {
-
-    _managerRef.once().then((DataSnapshot snapshot){
-      
+    _managerRef.once().then((DataSnapshot snapshot) {
       setState(() {
         managerList = new Map<String, dynamic>.from(snapshot.value);
         managerKeys = managerList.keys.toList();
         managerHintText = "Choose Manager";
       });
-      
     });
   }
 
@@ -100,15 +92,15 @@ class _AddUSerState extends State<AddUser> {
                         hintText: "Employee ID",
                         hintStyle: TextStyle(color: Colors.grey[400])),
                     validator: (value) {
-                        String ret;
-                        ret = value.isEmpty ? 'Employee ID can\'t be empty' : null;
-                        try{
-                          int.parse(value);
-                        }
-                        catch(e){
-                          ret = "Enter numeric value only";
-                        }
-                        return ret;
+                      String ret;
+                      ret =
+                          value.isEmpty ? 'Employee ID can\'t be empty' : null;
+                      try {
+                        int.parse(value);
+                      } catch (e) {
+                        ret = "Enter numeric value only";
+                      }
+                      return ret;
                     },
                     onSaved: (value) => UID = int.parse(value),
                   ),
@@ -170,13 +162,14 @@ class _AddUSerState extends State<AddUser> {
                         hintText: "Phone Number",
                         hintStyle: TextStyle(color: Colors.grey[400])),
                     validator: (value) {
-                        String ret;
-                        ret = value.isEmpty ? 'Phone Number can\'t be empty' : null;
+                      String ret;
+                      ret =
+                          value.isEmpty ? 'Phone Number can\'t be empty' : null;
 
-                        if(value.length != 10)
+                      if (value.length != 10)
                         ret = "Enter 10-digit phone number";
 
-                        return ret;
+                      return ret;
                     },
                     onSaved: (value) => PhoneNumber = value,
                   ),
@@ -197,118 +190,139 @@ class _AddUSerState extends State<AddUser> {
                   padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
                   child: DropdownButton<String>(
                     hint: Text(officeHintText),
-                  value: allotted_office,
-                  iconSize: 24,
-                  elevation: 16,
-                  underline: Container(
-                    height: 2,
-                  ),
-                  onChanged: (String newValue) {
-                    setState(() {
-                      print(newValue);
-                      allotted_office = newValue;
-                    });
-                  },
-                  items: officeKeys != null ? officeKeys.
-                          map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(officeList[value]["name"]),
-                      );
-                  }
-                ).toList() : [],
+                    value: allotted_office,
+                    iconSize: 24,
+                    elevation: 16,
+                    underline: Container(
+                      height: 2,
+                    ),
+                    onChanged: (String newValue) {
+                      setState(() {
+                        print(newValue);
+                        allotted_office = newValue;
+                      });
+                    },
+                    items: officeKeys != null
+                        ? officeKeys
+                            .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(officeList[value]["name"]),
+                            );
+                          }).toList()
+                        : [],
                   ),
                 ),
                 Container(
                   padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
                   child: DropdownButton<String>(
                     hint: Text(managerHintText),
-                  value: manager,
-                  iconSize: 24,
-                  elevation: 16,
-                  underline: Container(
-                    height: 2,
-                  ),
-                  onChanged: (String newValue) {
-                    setState(() {
-                      print(newValue);
-                      manager = newValue;
-                    });
-                  },
-                  items: managerKeys != null ? managerKeys.
-                          map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(managerList[value]["name"]),
-                      );
-                  }
-                ).toList() : [],
+                    value: manager,
+                    iconSize: 24,
+                    elevation: 16,
+                    underline: Container(
+                      height: 2,
+                    ),
+                    onChanged: (String newValue) {
+                      setState(() {
+                        print(newValue);
+                        manager = newValue;
+                      });
+                    },
+                    items: managerKeys != null
+                        ? managerKeys
+                            .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(managerList[value]["name"]),
+                            );
+                          }).toList()
+                        : [],
                   ),
                 ),
-
-                Container(padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
-                child: _errorMessage!=null ? (Text(_errorMessage)): Container(),
-                ),
-
                 Container(
-                  padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
-                  child: Center(
-                    child: RaisedButton(
-                      child: Text("Add Employee"),
-                      onPressed: validateEmployee,)
-                  )
-                )
+                  padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+                  child: _errorMessage != null
+                      ? (Text(_errorMessage))
+                      : Container(),
+                ),
+                Container(
+                    padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                    child: Center(
+                        child: RaisedButton(
+                      child: Text("Add Employee", style: TextStyle(color: Colors.white)),
+                      onPressed: () => validateEmployee(context),
+                      color: Colors.blue,
+                    ))),
+                     Container(
+                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
+                    child: Center(
+                        child: RaisedButton(
+                      child: Text("Reset Form", style: TextStyle(color: Colors.white)),
+                      onPressed: () { _errorMessage = null; _formKey.currentState.reset();},
+                      color: Colors.red,
+                    )))
               ],
             )),
       ),
     );
   }
 
-  void validateEmployee(){
-
+  void validateEmployee(context) {
     //_formKey.currentState.reset();
 
-    if(_formKey.currentState.validate()){
-      
-      if(this.manager == null)
-      {
+    if (_formKey.currentState.validate()) {
+      if (this.manager == null) {
         _errorMessage = "Please select a manager value";
         return;
       }
 
-      if(this.allotted_office == null)
-      {
+      if (this.allotted_office == null) {
         _errorMessage = "Please select an office value";
         return;
       }
     }
 
     _formKey.currentState.save();
-    createUser();
-
+    createUser(context);
   }
 
-  void createUser() async {
-    Employee employee = Employee(this.UID, this.Name, this.UUID, this.PhoneNumber, this.Address, this.allotted_office, this.manager);
+  void createUser(BuildContext context) async {
+    Employee employee = Employee(this.UID, this.Name, this.UUID,
+        this.PhoneNumber, this.Address, this.allotted_office, this.manager);
 
-    try{
+    try {
       print("in try");
-    authObject.signUp(this.email, this.password).then((Map<int, String> value){
-      
-      if(value[0] == 0){
-      _userRef.child(value[1]).set(employee.toJson());
-      _employeeIDRef.child(this.UID.toString()).set(this.email);
-      }
-      else
-      {
-        setState(() {
-          _errorMessage = value[1];
-        });
-        
-      }
-    });
-    }
-    catch(e){
+      authObject
+          .signUp(this.email, this.password)
+          .then((Map<int, String> value) {
+
+            print(value.keys.first);
+
+        if (value.keys.first == 0) {
+          _userRef.child(value[0]).set(employee.toJson());
+          _employeeIDRef.child(this.UID.toString()).set(this.email);
+
+          showDialog(
+            context: context,
+              child: AlertDialog(
+            title: Text("User Added Successfully"),
+            content: Text(
+                "User can start using his account through his employee ID and password."),
+          ));
+        } else {
+          setState(() {
+            showDialog(
+              context: context,
+                child: AlertDialog(
+              title: Text("Some Error Occured"),
+              content: value[1]!=null ? Text(
+                  value[1]):Text(" "),
+            ));
+          });
+        }
+      });
+    } catch (e) {
       print("in catch");
       print(e.message);
     }
